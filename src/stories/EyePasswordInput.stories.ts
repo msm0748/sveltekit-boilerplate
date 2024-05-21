8fc2d4d3af1fb603585f7aaf2cf93f6db92f136c
seokmin <msm0748@gmail.com>
2024-05-21 13:43:04 +0900

    feat: EyePassword 스토리북 추가

---
+++ src/stories/EyePasswordInput.stories.ts
@@ -0,0 +1,24 @@
+import EyePasswordInput from '$lib/components/EyePasswordInput.svelte';
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+	title: 'Example/EyePasswordInput',
+	component: EyePasswordInput,
+	tags: ['autodocs'],
+	argTypes: {
+		label: { control: 'text' },
+		value: { control: 'text' }
+	}
+} satisfies Meta<EyePasswordInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+	args: {
+		label: 'Password',
+		value: 'password'
+	}
+};
